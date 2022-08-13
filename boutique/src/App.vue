082d3a77b94c6b88f454d347896a191f611c905c
cynamon jules <julescweb@mail.com>
2022-08-13 19:18:30 +0200

    mise en place

---
+++ boutique/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Bonjour le monde !</h1>
+</template>
+
+<style lang="scss">
+@import "./assets/base.scss";
+@import "./assets/debug.scss";
+</style>
