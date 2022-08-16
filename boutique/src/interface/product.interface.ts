de57f8e700b872a92a32f3a49c4fd17546ce4a92
cynamon jules <julescweb@mail.com>
2022-08-16 15:40:54 +0200

    mise en place du filtre interface

--- boutique/src/interface/product.interface.ts
+++ boutique/src/interface/product.interface.ts
@@ -1,7 +1,10 @@
+import type { Category } from "./filters.interface";
+
 export interface ProductInterface {
   id: number;
   title: string;
   image: string;
   price: number;
   description: string;
+  category: Category;
 }
