867b1c0ed1b02711829e754ccee4030243fdba9f
cynamon jules <julescweb@mail.com>
2022-08-18 14:13:26 +0200

    refactorisation pour acceuillir la partie admin

--- boutique/src/App.vue
+++ boutique/src/App.vue
@@ -1,113 +1,15 @@
 <script setup lang="ts">
 import TheHeader from "./components/Header.vue";
 import TheFooter from "./components/Footer.vue";
-import Shop from "./components/Shop/Shop.vue";
-import Cart from "./components/Cart/Cart.vue";
-import data from "./Data/product";
-import { computed } from "vue";
-import { reactive } from "vue";
-import type {
-  ProductInterface,
-  ProductCartInterface,
-  FiltersInterface,
-  FilterUpdate,
-} from "./interface";
-import { DEFAULT_FILTERS } from "./Data/filters";
-
-const state = reactive<{
-  products: ProductInterface[];
-  cart: ProductCartInterface[];
-  filters: FiltersInterface;
-}>({
-  products: data,
-  cart: [],
-  filters: { ...DEFAULT_FILTERS },
-});
-
-function addProductToCart(productId: number): void {
-  const product = state.products.find((product) => product.id === productId);
-  if (product) {
-    const productInCart = state.cart.find(
-      (product) => product.id === productId
-    );
-    if (productInCart) {
-      productInCart.quantity++;
-    } else {
-      state.cart.push({
-        ...product,
-        quantity: 1,
-      });
-    }
-  }
-}
-
-function removeProductFromCart(productId: number): void {
-  const productFromCart = state.cart.find(
-    (product) => product.id === productId
-  );
-  if (productFromCart) {
-    if (productFromCart.quantity === 1) {
-      state.cart = state.cart.filter((product) => product.id !== productId);
-    } else {
-      productFromCart.quantity--;
-    }
-  }
-}
-
-const cartEmpty = computed(() => state.cart.length === 0);
-
-const firlteredProduct = computed(() => {
-  return state.products.filter((product) => {
-    if (
-      product.title
-        .toLocaleLowerCase()
-        .startsWith(state.filters.search.toLocaleLowerCase()) &&
-      product.price >= state.filters.priceRange[0] &&
-      product.price <= state.filters.priceRange[1] &&
-      (product.category === state.filters.category ||
-        state.filters.category === "all")
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-});
-
-function updateFilter(filterUpdate: FilterUpdate) {
-  if (filterUpdate.search !== undefined) {
-    state.filters.search = filterUpdate.search;
-  } else if (filterUpdate.priceRange) {
-    state.filters.priceRange = filterUpdate.priceRange;
-  } else if (filterUpdate.category) {
-    state.filters.category = filterUpdate.category;
-  } else {
-    state.filters = DEFAULT_FILTERS;
-  }
-}
+import Boutique from "./features/boutique/Boutique.vue";
 </script>
 
 <template>
-  <div
-    class="app-container"
-    :class="{
-      gridEmpty: cartEmpty,
-    }"
-  >
+  <div class="app-container">
     <TheHeader class="header" />
-    <Shop
-      @update-filter="updateFilter"
-      :filters="state.filters"
-      :products="firlteredProduct"
-      @add-product-to-cart="addProductToCart"
-      class="shop"
-    />
-    <Cart
-      v-if="!cartEmpty"
-      :cart="state.cart"
-      class="cart"
-      @remove-product-from-cart="removeProductFromCart"
-    />
+    <div class="app-content">
+      <Component :is="Boutique" />
+    </div>
     <TheFooter class="footer" />
   </div>
 </template>
@@ -119,27 +21,18 @@ function updateFilter(filterUpdate: FilterUpdate) {
 .app-container {
   min-height: 100vh;
   display: grid;
-  grid-template-areas: "header header" "shop cart" "footer footer";
-  grid-template-columns: 75% 25%;
+  grid-template-areas: "header" "app-content" "footer";
   grid-template-rows: 48px auto 48px;
-}
-
-.gridEmpty {
-  grid-template-areas: "header" "shop" "footer";
-  grid-template-columns: 100%;
 }
 
 .header {
   grid-area: header;
 }
-.shop {
-  grid-area: shop;
+
+.app-content {
+  grid-area: app-content;
 }
-.cart {
-  grid-area: cart;
-  border-left: var(--border);
-  background-color: white;
-}
+
 .footer {
   grid-area: footer;
 }
